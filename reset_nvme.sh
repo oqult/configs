85281effceee3c4902dd60a188d96726ca7930eb
Johnny Norrman <99218319+oqult@users.noreply.github.com>
2026-07-13 22:49:44 +0200

    Add script to reset NVMe device
    Script to reset NVMe device by unmounting, removing, and rescanning.

---
+++ reset_nvme.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+DEV=nvme1n1   # ändra till ditt faktiska devicenamn
+PCI=$(basename $(readlink -f /sys/block/$DEV/device/device))
+sudo umount /dev/${DEV}* 2>/dev/null
+echo 1 | sudo tee /sys/bus/pci/devices/$PCI/remove
+sleep 2
+echo 1 | sudo tee /sys/bus/pci/rescan
+sudo mount -a
+sleep 2
+lsblk | grep nvme
